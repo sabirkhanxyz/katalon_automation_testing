95785604a309e2b07bf6c70406e5392ebf7ba653
sabirkhanxyz <sabirkhanxyz314@gmail.com>
2026-08-18 16:59:30 +0500

    hello

--- Test Suites/open browser.ts
+++ Test Suites/open browser.ts
@@ -12,17 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6df549ad-0fe1-4040-9fac-7daa762f86f8</testSuiteGuid>
    <testCaseLink>
-      <guid>48ec7941-b650-4412-9c48-2aa3c840d28e</guid>
+      <guid>cb8f2e33-4805-426f-9d65-02a277cf1fc2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Test Scripting mod</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>9c933e91-41df-4e3c-aefb-06f770b308ee</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Manual_Test</testCaseId>
+      <testCaseId>Test Cases/Web_Recoder_demo</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
