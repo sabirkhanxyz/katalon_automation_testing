31c23381e5bb41df27540c4c595f548ba51a1739
sabirkhanxyz <sabirkhanxyz314@gmail.com>
2026-08-18 16:36:11 +0500

    update new cases

--- Test Suites/open browser.ts
+++ Test Suites/open browser.ts
@@ -12,10 +12,17 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>6df549ad-0fe1-4040-9fac-7daa762f86f8</testSuiteGuid>
    <testCaseLink>
-      <guid>549f3cc7-50b8-4877-a625-14a8d4714bb3</guid>
+      <guid>48ec7941-b650-4412-9c48-2aa3c840d28e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/add new test case</testCaseId>
+      <testCaseId>Test Cases/Test Scripting mod</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9c933e91-41df-4e3c-aefb-06f770b308ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manual_Test</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
